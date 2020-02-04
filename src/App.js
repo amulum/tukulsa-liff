ac49955a38129cd2400e293ab1bc9df4af1e9d0f
Achmad Miftahul Ulum <miftahul@alterra.id>
2020-02-04 21:09:35 +0700

    cek redirect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import { Redirect } from 'react-router-dom';
+
+const liff = window.liff;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayName : '',
+      userId : '',
+      pictureUrl : '',
+      statusMessage : ''
+    };
+
+    this.initialize = this.initialize.bind(this);
+    this.closeApp = this.closeApp.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('load', this.initialize);
+  }
+
+  initialize() {
+    liff.init(async (data) => {
+      let profile = await liff.getProfile();
+      this.setState({
+        displayName : profile.displayName,
+        userId : profile.userId,
+        pictureUrl : profile.pictureUrl,
+        statusMessage : profile.statusMessage
+      });
+    }); 
+  }
+
+  closeApp(event) {
+    event.preventDefault();
+    liff.sendMessages([{
+      type: 'text',
+      text: "Bye Bye!!!"
+    }]).then(() => {
+      liff.closeWindow();
+    });
+  }
+
+  render() {
+    return (
+      <Redirect to="https://app.sandbox.midtrans.com/snap/v2/vtweb/b3ff18b5-617a-4d88-bcae-4c8db81e1255" />
+      // <div className="container">
+      //   <div className="columns m-t-10">
+      //     <div className="column col-xs-12">
+      //       <div className="panel">
+      //         <div className="panel-header text-center">
+      //           <figure className="avatar avatar-lg">
+      //             <img src={this.state.pictureUrl} alt="Avatar" />
+      //           </figure>
+      //           <div className="panel-title h5 mt-10">{this.state.displayName}</div>
+      //           <div className="panel-subtitle">{this.state.statusMessage}</div>
+      //         </div>
+      //         <div className="panel-footer">
+      //           <button className="btn btn-primary btn-block" onClick={this.closeApp}>Close</button>
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
+    );
+  }
 }
 
 export default App;
