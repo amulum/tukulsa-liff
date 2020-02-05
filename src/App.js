522d960ce5915b4057432d932807df0420e7c46a
Achmad Miftahul Ulum <miftahul@alterra.id>
2020-02-05 16:10:54 +0700

    test broiler

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import Mainroute from './Routes/Mainroute'
+// import React from 'react';
+// import Mainroute from './Routes/Mainroute'
 
-function App() {
+// function App() {
+//     return (
+//       <Mainroute />
+//     );
+// }
+
+// export default App;
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import Profile from './pages/Profile';
+import SendMessage from './pages/SendMessage';
+import LIFFWindow from './pages/LIFFWindow';
+import Header from './pages/components/Header';
+
+class App extends Component {
+  render() {
     return (
-      <Mainroute />
+      <div className="app">
+        <header className="app-header">
+          <h3 className="app-title">React LIFF Boilerplate</h3>
+        </header>
+        <Header />
+        <Router>
+          <div>
+            <Route exact path="/" component={Profile} />
+            <Route path="/message" component={SendMessage} />
+            <Route path="/window" component={LIFFWindow} />
+          </div>
+        </Router>
+      </div>
     );
+  }
 }
 
 export default App;
