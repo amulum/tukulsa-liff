6cb9d62b83620833fda2a4b8fe7c77f14ad6a896
Achmad Miftahul Ulum <miftahul@alterra.id>
2020-02-05 07:59:15 +0700

    cobain listen port

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// liff ID
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const myLiffId = process.env.MY_LIFF_ID;
+
+app.use(express.static('public'));
+
+app.get('/send-id', function(req, res) {
+    res.json({id: myLiffId});
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
